e739627d8a45588e5b5f53df432a3561f7bb680c
Luca Sbardella <luca@quantmind.com>
2016-02-18 19:15:59 +0000

    removed config

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ['GITHUB_USERNAME'] = "qmboot"
-os.environ['GITHUB_TOKEN'] = "3bab62f11d640c810926bdb09d5e2b0c443d0352"
-os.environ['GITHUB_TEST_REPO'] = "quantmind/pybriciole"
